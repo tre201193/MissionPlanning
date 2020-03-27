f71484e35afb0740200b6e83a3dc8339fbb32c7d
tre201193 <61996174+tre201193@users.noreply.github.com>
2020-03-26 22:58:34 -0500

    finished part 1 of studio-1

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './header/crew/crew.component';
+import { EquipmentComponent } from './header/equipment/equipment.component';
+import { ExperimentsComponent } from './header/experiments/experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExperimentsComponent
   ],
   imports: [
     BrowserModule
